2d8eb22b45a3309ec3ab3f810eae32f592143267
Sungpil Han <pil.han@gmail.com>
2016-08-08 11:40:08 +0900

    graph.R version 1.0
    그래프 그리는거 업뎃했으니 참고하세요.

--- graph.R
+++ graph.R
@@ -1,1 +1,22 @@
-# ¼­ºêÀÎÅÏ PK ±×·¡ÇÁ ±×¸±°Å¿¡¿ä.
+# ë¶ˆëŸ¬ì˜¤ê¸°
+
+setwd("D:/Subintern/WNL")
+PKRaw = read.csv("Drug_X_PK.csv")
+
+# There's missing ID so we implemented this code.
+UniID = unique(PKRaw$ID)
+
+# 1st ëŒ€ìƒìì— ëŒ€í•œ ê·¸ë¦¼
+for (i in 1:length(UniID)){
+  PKRaw01 = PKRaw[PKRaw$ID == UniID[i], ]
+  plot(PKRaw01$TIME, PKRaw01$DV, main = paste("SUBJID ", UniID[i], sep = ""))
+}
+
+# ggplot
+library(ggplot2)
+
+for (i in 1:length(UniID)){
+  PKRaw01 = PKRaw[PKRaw$ID == UniID[i], ]
+  plot(ggplot(PKRaw01, aes(x = TIME, y = DV)) + geom_point() + geom_line() + ggtitle(paste("SUBJID ", UniID[i], sep = "")))
+}
+

00f998a92d7a30907f33d347bccba138261cf3ce
Sungpil Han <pil.han@gmail.com>
2016-08-08 12:11:21 +0900

    커밋

--- graph.R
+++ graph.R
@@ -11,11 +11,15 @@ for (i in 1:length(UniID)){
   plot(PKRaw01$TIME, PKRaw01$DV, main = paste("SUBJID ", UniID[i], sep = ""))
 }
 
-# ggplot2
+# ggplot2 - this will be used for docx file.
 library(ggplot2)
 
-for (i in 1:length(UniID)){
+for (i in 1:2){ #length(UniID)){
   PKRaw01 = PKRaw[PKRaw$ID == UniID[i], ]
-  plot(ggplot(PKRaw01, aes(x = TIME, y = DV)) + geom_point() + geom_line() + ggtitle(paste("SUBJID ", UniID[i], sep = "")))
+  FINAL = ggplot(PKRaw01, aes(x = TIME, y = DV)) + geom_point() + geom_line() + ggtitle(paste("SUBJID ", UniID[i], sep = ""))
+  plot(FINAL)
+  # ggsave(filename = paste0("Figures/SUBJID",UniID[i], ".jpg"), FINAL)
 }
 
+# y축 범위를 고정할 것. x축 y축 TIME -> label Time (hour), DV -> Concentration (ug/L)
+# Title에 Dose 정보를 삽입 SUBJID ? (~~ mg)
